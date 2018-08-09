5e8971a6d1e7ef4c021417dadea8a64cf5b27f75
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-08-10 01:31:27 +0530

    Updated meta tags

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PHP Terminal</title>
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body>
-    <div id="output"></div>
-    <div class="input-container">
-      <input type="text" id="command"/>
-    </div>
-    <script src="/js/main.js"></script>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="PHP Terminal">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript,PHP">
+  <meta name="author" content="Bhaktij Koli <bhaktijkoli121@gmail.com>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PHP Terminal</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <div id="output"></div>
+  <div class="input-container">
+    <input type="text" id="command"/>
+  </div>
+  <script src="/js/main.js"></script>
+</body>
 </html>
